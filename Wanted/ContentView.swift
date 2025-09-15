5055766ae4cfccb745c6167bcf2db916d3eac31b
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-09-15 15:27:24 +0200

    Add renderer on contentView

--- Wanted/ContentView.swift
+++ Wanted/ContentView.swift
@@ -9,11 +9,33 @@ import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var renderedPoster: Image?
 
     var body: some View {
-        VStack {
-            Text("Hello World!")
+        NavigationStack {
+            Form {
+                renderedPoster?
+                    .resizable()
+                    .scaledToFit()
+            }
+            .navigationTitle("Design your poster")
+            .navigationBarTitleDisplayMode(.inline)
         }
+        .onAppear(perform: render)
+    }
+
+    func render() {
+        let renderer = ImageRenderer(
+            content: WantedPosterView(
+                crime: "Horse Rustling",
+                reward: "$500",
+                contact: "Turn yourself in, thief!",
+                paperOpacity: 0.75))
+
+        if let image = renderer.uiImage {
+            renderedPoster = Image(uiImage: image)
+        }
+
     }
 }
 
