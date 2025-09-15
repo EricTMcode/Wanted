647d5474e4b786b287dbc1af7fa0f75575edb901
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-09-15 14:44:55 +0200

    Complet project

---
+++ Wanted/WantedApp.swift
@@ -0,0 +1,17 @@
+//
+//  WantedApp.swift
+//  Wanted
+//
+//  Created by Eric on 11/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct WantedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
